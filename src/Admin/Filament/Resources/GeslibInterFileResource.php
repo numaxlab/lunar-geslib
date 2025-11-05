127f56fb5266040643d88dcd226f4113d0a5e205
ablunier <4113587+ablunier@users.noreply.github.com>
2025-11-05 13:29:35 +0000

    fix code style

--- src/Admin/Filament/Resources/GeslibInterFileResource.php
+++ src/Admin/Filament/Resources/GeslibInterFileResource.php
@@ -44,7 +44,7 @@ class GeslibInterFileResource extends Resource
                     ->searchable()
                     ->sortable()
                     ->badge()
-                    ->color(fn(string $state): string => match ($state) {
+                    ->color(fn (string $state): string => match ($state) {
                         GeslibInterFile::STATUS_SUCCESS => 'success',
                         GeslibInterFile::STATUS_FAILED => 'danger',
                         GeslibInterFile::STATUS_WARNING => 'warning',
@@ -88,17 +88,15 @@ class GeslibInterFileResource extends Resource
                 Tables\Actions\Action::make('view_log')
                     ->label('Ver log')
                     ->icon('heroicon-o-eye')
-                    ->modalContent(fn($record,
-                    ): \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory
-                        => view('lunar-geslib::filament.modals.geslib-inter-file-log',
+                    ->modalContent(fn ($record,
+                    ): \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory => view('lunar-geslib::filament.modals.geslib-inter-file-log',
                         [
                             'log' => $record->log,
                         ]))
                     ->modalHeading('Registro de actividad')
                     ->modalSubmitAction(false)
-                    ->modalCancelAction(fn(StaticAction $action,
-                    ): \Filament\Actions\StaticAction
-                        => $action->label('Cerrar')),
+                    ->modalCancelAction(fn (StaticAction $action,
+                    ): \Filament\Actions\StaticAction => $action->label('Cerrar')),
             ])
             ->defaultSort('created_at', 'desc');
     }
--- src/Jobs/ProcessGeslibInterFileBatchLine.php
+++ src/Jobs/ProcessGeslibInterFileBatchLine.php
@@ -90,7 +90,7 @@ class ProcessGeslibInterFileBatchLine implements ShouldBeUnique, ShouldQueue
         $this->batchLine->delete();
 
         if ($batchLines->count() > 1) {
-            $nextBatchLine = $batchLines->first(fn($line) => $line->id !== $this->batchLine->id);
+            $nextBatchLine = $batchLines->first(fn ($line) => $line->id !== $this->batchLine->id);
 
             if ($nextBatchLine) {
                 self::dispatch($this->geslibInterFile, $nextBatchLine);
@@ -107,11 +107,11 @@ class ProcessGeslibInterFileBatchLine implements ShouldBeUnique, ShouldQueue
 
     private function getStatusFromLog(array $log): string
     {
-        if (array_any($log, fn($line) => $line['level'] === CommandContract::LEVEL_ERROR)) {
+        if (array_any($log, fn ($line) => $line['level'] === CommandContract::LEVEL_ERROR)) {
             return GeslibInterFile::STATUS_FAILED;
         }
 
-        if (array_any($log, fn($line) => $line['level'] === CommandContract::LEVEL_WARNING)) {
+        if (array_any($log, fn ($line) => $line['level'] === CommandContract::LEVEL_WARNING)) {
             return GeslibInterFile::STATUS_WARNING;
         }
 
